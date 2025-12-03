4d9c0f3a958676e5993bf666fc2da93897e35baa
Elian <elifbaque@gmail.com>
2025-12-03 18:18:47 -0300

    Elimino backend para separar

--- main-backend/apitoken/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApitokenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apitoken'
--- main-backend/backend/settings.py
+++
@@ -1,6 +0,0 @@
-# This bring the settings from the local_settings.py file
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
